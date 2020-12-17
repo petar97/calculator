7dc96f22d257bd39b4c8689ab18fbf3881a45d94
Petar Milovanovic <petar97m@gmail.com>
2020-12-17 13:04:20 +0100

    Simplified and minimized functions and code overall, added decimal limit and made a few style changes

--- main.js
+++ main.js
@@ -16,32 +16,59 @@ function devide(a, b) {
 
 function operate(operator, a, b) {
     switch (operator) {
-        case "plus":
+        case "addition":
             return add(a, b);
             break;
 
-        case "minus":
+        case "subtraction":
             return subtract(a, b);
             break;
 
-        case "multiplier":
+        case "multiplication":
             return multiply(a, b);
             break;
 
-        case "divider":
+        case "division":
             return devide(a, b);
             break;
     }
 
 }
 
+function roundToTwo(num) {
+    if (num % 1 == 0) {
+        return num;
+    } else {
+        return +(Math.round(num + "e+2")  + "e-2");
+    }
+}
+
+function onClickOperator(operatorSign) {
+    if (a != undefined) {
+        if (result) {
+            tempDisplay.textContent = mainDisplay.textContent;
+            mainDisplay.textContent = "";
+            result = undefined;
+        } else {
+            b = Number(displayValue);
+            result = operate(operator, a, b);
+            tempDisplay.textContent = roundToTwo(result);
+            mainDisplay.textContent = "";
+            a = result;
+            result = undefined;
+        }
+    } else {
+        a = Number(displayValue);
+        mainDisplay.textContent = "";
+        tempDisplay.textContent = a;
+    }
+    operator = operatorSign.id;
+}
+
 const mainDisplay = document.querySelector("#main-display");
 const tempDisplay = document.querySelector("#temp-display");
 const numbers = Array.from(document.querySelectorAll(".number"));
-const addition = document.querySelector("#addition");
-const subtraction = document.querySelector("#subtraction");
-const multiplication = document.querySelector("#multiplication");
-const division = document.querySelector("#division");
+const operators = Array.from(document.querySelectorAll(".operator"))
 const operation = document.querySelector("#operation");
 const clear = document.querySelector("#clear");
 
@@ -52,115 +79,56 @@ let result;
 let displayValue;
 let operator;
 
+mainDisplay.textContent = "0";
 
-window.addEventListener("click", (e) => {
-    /* if (e.target.className == "number") {
-        if (!(regex.test(display.textContent))) {
-            display.textContent = "";
-        }
-
-        display.textContent += e.target.textContent;
-        displayValue = display.textContent;
-        console.log(displayValue);
-    } */
-
-    if (e.target.className === "number") {
-        mainDisplay.textContent += e.target.textContent;
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        if (a == undefined) {
+            mainDisplay.textContent = "";
+        } 
+        mainDisplay.textContent += number.textContent;
         displayValue = mainDisplay.textContent;
-        //console.log(displayValue);
-    }
+    })
 });
 
-addition.addEventListener("click", () => {
-    if (a != undefined) {
-        if (result) {
-            mainDisplay.textContent = "";
-            tempDisplay.textContent = result;
-        } else {
-            b = Number(displayValue);
-            result = operate(operator, a, b);
-            tempDisplay.textContent = result;
-            mainDisplay.textContent = "";
-            a = result;
-        }
-    } else {
-        a = Number(displayValue);
-        mainDisplay.textContent = "";
-    }
-    operator = "plus";
-});
 
-subtraction.addEventListener("click", () => {
-    if (a != undefined) {
-        if (result) {
-            mainDisplay.textContent = "";
-            tempDisplay.textContent = result;
-        } else {
-            b = Number(displayValue);
-            result = operate(operator, a, b);
-            tempDisplay.textContent = result;
-            mainDisplay.textContent = "";
-            a = result;
-        }
-    } else {
-        a = Number(displayValue);
-        mainDisplay.textContent = "";
-    }
-    operator = "minus";
-});
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        switch(operator.id) {
+            case "addition":
+                onClickOperator(operator);
+                break;
 
-multiplication.addEventListener("click", () => {
-    if (a != undefined) {
-        if (result) {
-            mainDisplay.textContent = "";
-            tempDisplay.textContent = result;
-        } else {
-            b = Number(displayValue);
-            result = operate(operator, a, b);
-            tempDisplay.textContent = result;
-            mainDisplay.textContent = "";
-            a = result;
-        }
-    } else {
-        a = Number(displayValue);
-        mainDisplay.textContent = "";
-    }
-    operator = "multiplier";
-});
+            case "subtraction":
+                onClickOperator(operator);
+                break;
 
-division.addEventListener("click", () => {
-    if (a != undefined) {
-        if (result) {
-            mainDisplay.textContent = "";
-            tempDisplay.textContent = result;
-        } else {
-            b = Number(displayValue);
-            result = operate(operator, a, b);
-            tempDisplay.textContent = result;
-            mainDisplay.textContent = "";
-            a = result;
+             case "multiplication":
+                onClickOperator(operator);
+                break;
+                        
+            case "division":
+                onClickOperator(operator);
+                break;
+                                    
         }
-    } else {
-        a = Number(displayValue);
-        mainDisplay.textContent = "";
-    }
-    operator = "divider";
+    })
 });
 
 operation.addEventListener("click", () => {
     tempDisplay.textContent = "";
     b = Number(displayValue);
-    mainDisplay.textContent = "";
     result = operate(operator, a, b);
-    mainDisplay.textContent = result;
+    mainDisplay.textContent = roundToTwo(result);
     a = result;
 });
 
 clear.addEventListener("click", () => {
-    mainDisplay.textContent = "";
+    mainDisplay.textContent = "0";
     tempDisplay.textContent = "";
     a = undefined;
     b = undefined;
+    result = undefined;
 });
 
 window.addEventListener("click", (e) => {
